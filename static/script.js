01d0465dec55c8d579e38af50d78dbff444f5077
msz89 <matthewszwec@gmail.com>
2021-10-03 10:44:12 +1100

    added collapsible section and recoloured

--- static/script.js
+++ static/script.js
@@ -1,26 +1,22 @@
-/**
- * Copyright 2018, Google LLC
- * Licensed under the Apache License, Version 2.0 (the `License`);
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an `AS IS` BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
-// [START gae_python38_log]
-// [START gae_python3_log]
-'use strict';
+// 'use strict';
 
 window.addEventListener('load', function () {
 
   console.log("js Test");
 
 });
-// [END gae_python3_log]
-// [END gae_python38_log]
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+} 
